01f58c0ec0bbb1d170555b119ab867524b6c083a
prukon <prukon@gmail.com>
2024-01-10 23:07:10 +0000

    fix sintax

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
-
-
+        
         $teams = Team::find(1);
         dd($teams);
         return "this dashboard";
