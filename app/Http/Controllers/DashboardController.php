e9658d105402cbb2db70eede7e90a45ac4c738cc
prukon <prukon@gmail.com>
2024-01-10 23:05:18 +0000

    add Teams migration

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
+
+
+        $teams = Team::find(1);
+        dd($teams);
         return "this dashboard";
+
     }
 }
