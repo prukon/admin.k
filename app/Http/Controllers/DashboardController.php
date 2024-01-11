156a4dc1bcc79e1eec4a597e2ace1838623eeab3
prukon <prukon@gmail.com>
2024-01-11 22:40:27 +0000

    edit migration user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Team;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -10,7 +11,8 @@ class DashboardController extends Controller
     public function index() {
 
         $teams = Team::find(1);
-        dd($teams);
+        $user  = User::find(1);
 
+        dd($user);
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
+            $table->Integer('team_id');
+            $table->boolean('is_enabled')->default(1);
+            $table->string('image')->nullable();
+            $table->string('image_crop')->nullable();
+            $table->string('birthday')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
