6c06bbba180607f61c2eac4bb230b24796646c70
prukon <prukon@gmail.com>
2024-08-22 23:28:36 +0000

    add model and migration for Order page

---
+++ app/Models/Order.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users_prices'; //явное указание к какой таблице в БД привязана модель
+    protected $guarded = []; //разрешение на изменение данных в таблице}
+}
+}
