7ef7b19c164d34ce1cac2770af32e0cab225913a
prukon <prukon@gmail.com>
2024-09-11 15:31:56 +0000

    add btn-for-pay service

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -25,37 +25,52 @@
                             <td class="col-4">Регистрация на сайте</td>
                             <td class="col-6">
                                 <div class="form-check">
-{{--                                    <input class="form-check-input" type="checkbox" value="" id="registrationActivity">--}}
+                                    {{--                                    <input class="form-check-input" type="checkbox" value="" id="registrationActivity">--}}
                                     <input class="form-check-input" type="checkbox" value="" id="registrationActivity"
                                             {{ $isRegistrationActivity ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="registrationActivity">Включена/Выключена</label>
+                                    <label class="form-check-label"
+                                           for="registrationActivity">Включена/Выключена</label>
 
 
                                 </div>
 
                             </td>
                             <td class="col-2">
-                                <button id="btnRegistrationActivity" class="btn btn-primary mb-3 mt-3">Применить</button>
+                                <button id="btnRegistrationActivity" class="btn btn-primary mb-3 mt-3">Применить
+                                </button>
                             </td>
                         </tr>
-
 
 
                         <tr>
                             <td class="col-4">Текст уведомления у пользователей</td>
                             <td colspan="col-6">
-{{--                    <textarea class=" form-control text-start" id="textForUsers" rows="3">--}}
-{{--                        {{$textForUsers}}--}}
-{{--                    </textarea>--}}
-                                <textarea class="form-control text-start" id="textForUsers" rows="3">{{ trim($textForUsers) }}</textarea>
-
+                                <textarea class="form-control text-start" id="textForUsers"
+                                          rows="3">{{ trim($textForUsers) }}</textarea>
                             </td>
                             <td class="col-2">
                                 <button id="btnTextForUsers" class="btn btn-primary mb-3 mt-3">Применить</button>
                             </td>
                         </tr>
-                        </thead>
+
+                        </tbody>
                     </table>
+
+                    <table class="table  table-bordered mt-5 ">
+                        <tbody>
+                        <tr>
+                            <td class="col-4">Оплата сервиса</td>
+                            <td colspan="col-6">
+                                <iframe src="https://yoomoney.ru/quickpay/fundraise/button?billNumber=1YJRbAR2lJs.230810&"
+                                        width="330" height="50" frameborder="0" allowtransparency="true"
+                                        scrolling="no"></iframe>
+                            </td>
+                            {{--                            <td class="col-2">--}}
+                            {{--                            </td>--}}
+                        </tr>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
