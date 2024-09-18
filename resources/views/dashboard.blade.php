e2c014c8d0f65904564ad6ac9164efcf1565976a
prukon <prukon@gmail.com>
2024-09-18 09:18:29 +0000

    fix format birthday in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -129,7 +129,7 @@
                                     -
                                 @endif </span></div>
                         <div class="birthday">Дата рождения:<span class="birthday-value"> @if($curUser->birthday)
-                                    {{$curUser->birthday}}
+                                    {{ \Carbon\Carbon::parse($curUser->birthday)->format('d.m.Y') }}
                                 @else
                                     -
                                 @endif </span></div>
