5b5fe0de3c6f71c2ddb6b0104985a301cd2e1374
prukon <prukon@gmail.com>
2024-09-18 08:57:05 +0000

    add reports and debt report

--- app/Http/Controllers/Admin/Report/ReportController.php
+++ app/Http/Controllers/Admin/Report/ReportController.php
@@ -7,7 +7,9 @@ use App\Models\Payment;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\DataTables;
+
 
 
 class ReportController extends Controller
@@ -73,31 +75,30 @@ class ReportController extends Controller
     public function getDebts(Request $request)
     {
         if ($request->ajax()) {
-            $payments = Payment::with(['user.team'])->get();
+            $usersWithUnpaidPrices = DB::table('users_prices')
+                ->leftJoin('users', 'users.id', '=', 'users_prices.user_id')
+                ->select('users.name as user_name','users.id as user_id' , 'users_prices.month', 'users_prices.price')
+                ->where('users_prices.is_paid', 0) 
+                ->where('users_prices.price', '>', 0)
+                ->where('users_prices.month', '<', 'Сентябрь 2024')
+                ->get();
 
-            return DataTables::of($payments)
+
+            // Добавляем проверку на наличие данных
+            if ($usersWithUnpaidPrices->isEmpty()) {
+                return response()->json(['error' => 'Данные не найдены'], 404);
+            }
+
+            return DataTables::of($usersWithUnpaidPrices)
                 ->addIndexColumn()
                 ->addColumn('user_name', function($row) {
-                    // Проверяем, есть ли пользователь в таблице payments
-                    return $row->user_name
-                        ? $row->user_name // Возвращаем имя пользователя из payments
-                        : ($row->user ? $row->user->name : 'Без пользователя'); // Или из связанной модели, если нет в payments
+                    return $row->user_name ? $row->user_name : 'Без имени'; // Проверяем наличие имени пользователя
                 })
-                ->addColumn('user_id', function($row) {
-                    // Возвращаем user_id, если он существует, иначе null
-                    return $row->user ? $row->user->id : null;
+                ->addColumn('month', function($row) {
+                    return $row->month; // Месяц
                 })
-                ->addColumn('team_title', function ($row) {
-                    // Проверка, существует ли пользователь и его команда
-                    return $row->user && $row->user->team
-                        ? $row->user->team->title // Возвращаем название команды
-                        : 'Без команды'; // Если команды нет
-                })
-                ->addColumn('summ', function ($row) {
-                    return number_format($row->summ, 0) . ' руб'; // Формат суммы
-                })
-                ->addColumn('operation_date', function ($row) {
-                    return $row->operation_date; // Дата операции
+                ->addColumn('price', function($row) {
+                    return number_format($row->price, 2) . ' руб'; // Формат цены
                 })
                 ->make(true);
         }
--- resources/views/admin/report/debt.blade.php
+++ resources/views/admin/report/debt.blade.php
@@ -1,4 +1,3 @@
-@extends('layouts.main2')
 @extends('layouts.admin2')
 @section('content')
 
@@ -27,91 +26,86 @@
                                 <thead>
                                 <tr>
                                     <th>№</th>
-                                    <th>Имя ученика</th>
-                                    <th>Группа</th>
-                                    <th>Сумма платежа</th>
-                                    <th>Оплаченный месяц</th>
-                                    <th>Дата и время платежа</th>
+                                    <th>Имя пользователя</th>
+                                    <th>Месяц</th>
+                                    <th>Сумма</th>
                                 </tr>
                                 </thead>
                             </table>
                         </div>
-                        @section('scripts')
-                            <script type="text/javascript">
-                                $(function () {
-                                    var table = $('#debts-table').DataTable({
-                                        processing: true,
-                                        serverSide: true,
-                                        ajax: "{{ route('debts.getDebts') }}",
-                                        columns: [
-                                            {data: 'DT_RowIndex', name: 'DT_RowIndex'},
-
-                                            {
-                                                data: null,
-                                                name: 'user_name',
-                                                render: function (data, type, row) {
-                                                    if (row.user_id && row.user_name) {
-                                                        return '<a href="/admin/users/' + row.user_id + '/edit">' + row.user_name + '</a>';
-                                                    } else {
-                                                        return row.user_name ? row.user_name : 'Без имени';
-                                                    }
-                                                }
-                                            },
-
-                                            {data: 'team_title', name: 'team_title'},
-                                            {data: 'summ', name: 'summ'},
-                                            {data: 'payment_month', name: 'payment_month'},
-                                            {
-                                                data: 'operation_date',
-                                                name: 'operation_date',
-                                                render: function (data, type, row) {
-                                                    if (data) {
-                                                        var date = new Date(data);
-                                                        var day = ("0" + date.getDate()).slice(-2);
-                                                        var month = ("0" + (date.getMonth() + 1)).slice(-2);
-                                                        var year = date.getFullYear();
-                                                        var hours = ("0" + date.getHours()).slice(-2);
-                                                        var minutes = ("0" + date.getMinutes()).slice(-2);
-                                                        var seconds = ("0" + date.getSeconds()).slice(-2);
-                                                        return day + '.' + month + '.' + year + ' / ' + hours + ':' + minutes + ':' + seconds;
-                                                    }
-                                                    return data;
-                                                }
-                                            }
-                                        ],
-                                        order: [[5, 'desc']], // Сортировка по столбцу "Дата" в порядке убывания
-
-                                        language: {
-                                            "processing": "Обработка...",
-                                            "search": "Поиск:",
-                                            "lengthMenu": "Показать _MENU_ записей",
-                                            "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
-                                            "infoEmpty": "Записи с 0 до 0 из 0 записей",
-                                            "infoFiltered": "(отфильтровано из _MAX_ записей)",
-                                            "loadingRecords": "Загрузка записей...",
-                                            "zeroRecords": "Записи отсутствуют.",
-                                            "emptyTable": "В таблице отсутствуют данные",
-                                            "paginate": {
-                                                "first": "Первая",
-                                                "previous": "Предыдущая",
-                                                "next": "Следующая",
-                                                "last": "Последняя"
-                                            },
-                                            "aria": {
-                                                "sortAscending": ": активировать для сортировки столбца по возрастанию",
-                                                "sortDescending": ": активировать для сортировки столбца по убыванию"
-                                            }
-                                        }
-                                    });
-                                });
-                            </script>
-
-                        @endsection
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        $(function () {
+            var table = $('#debts-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('debts.getDebts') }}",
+                columns: [
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+
+                    {
+                        data: null,
+                        name: 'user_name',
+                        render: function (data, type, row) {
+                            // Проверяем наличие user_id, если есть, делаем ссылку
+                            if (row.user_id) {
+                                return '<a href="/admin/users/' + row.user_id + '/edit">' + row.user_name + '</a>';
+                            } else {
+                                // Если user_id нет, просто выводим имя пользователя
+                                return row.user_name ? row.user_name : 'Без имени';
+                            }
+                        }
+                    },
+
+                    {data: 'month', name: 'month'},
+                    {
+                        data: 'price',
+                        name: 'price',
+                        render: function (data, type, row) {
+                            var price = parseFloat(row.price);
+                            if (!isNaN(price)) {
+                                return new Intl.NumberFormat('ru-RU', {
+                                    style: 'currency',
+                                    currency: 'RUB'
+                                }).format(price);
+                            } else {
+                                return 'Неверное значение';
+                            }
+                        }
+                    }
+                ],
+                order: [[3, 'desc']], // Сортировка по столбцу "Сумма" в порядке убывания
+
+                language: {
+                    "processing": "Обработка...",
+                    "search": "Поиск:",
+                    "lengthMenu": "Показать _MENU_ записей",
+                    "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+                    "infoEmpty": "Записи с 0 до 0 из 0 записей",
+                    "infoFiltered": "(отфильтровано из _MAX_ записей)",
+                    "loadingRecords": "Загрузка записей...",
+                    "zeroRecords": "Записи отсутствуют.",
+                    "emptyTable": "В таблице отсутствуют данные",
+                    "paginate": {
+                        "first": "Первая",
+                        "previous": "Предыдущая",
+                        "next": "Следующая",
+                        "last": "Последняя"
+                    },
+                    "aria": {
+                        "sortAscending": ": активировать для сортировки столбца по возрастанию",
+                        "sortDescending": ": активировать для сортировки столбца по убыванию"
+                    }
+                }
+            });
+        });
+    </script>
 @endsection
