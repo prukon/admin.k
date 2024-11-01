38bae6e08208522e408fa4aee47a3148e01e6409
prukon <prukon@gmail.com>
2024-11-02 02:44:49 +0300

    del team_id in user when del team

--- app/Http/Controllers/Admin/Team/DestroyController.php
+++ app/Http/Controllers/Admin/Team/DestroyController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin\Team;
 use App\Http\Controllers\Controller;
 use App\Models\Log;
 use App\Models\Team;
+use Illuminate\Support\Facades\DB;
+
 
 class DestroyController extends Controller
 {
@@ -12,23 +14,36 @@ class DestroyController extends Controller
     {
         $this->middleware('admin');
     }
-
+    
     public function __invoke(Team $team)
     {
         $authorId = auth()->id(); // Авторизованный пользователь
 
-        $team->delete();
+        DB::beginTransaction();
+        try {
+            // Обновляем пользователей, устанавливая team_id в null
+            \App\Models\User::where('team_id', $team->id)->update(['team_id' => null]);
 
-        Log::create([
-            'type' => 3, // Лог для обновления групп
-            'action' => 33,
-            'author_id' => $authorId,
-            'description' => "Удалена группа: {$team->title}.  ID: {$team->id}.",
-            'created_at' => now(),
-        ]);
+            // Удаляем группу
+            $team->delete();
 
-        return response()->json(['message' => 'Группа успешно удалена']);
+            // Логирование
+            Log::create([
+                'type' => 3, // Лог для обновления групп
+                'action' => 33,
+                'author_id' => $authorId,
+                'description' => "Удалена группа: {$team->title}. ID: {$team->id}.",
+                'created_at' => now(),
+            ]);
 
-//        return redirect()->route('admin.team.index');
+            DB::commit(); // Фиксируем транзакцию
+
+            return response()->json(['message' => 'Группа и ее связь с пользователями успешно удалены']);
+        } catch (\Exception $e) {
+            DB::rollBack(); // Откатываем транзакцию в случае ошибки
+
+            return response()->json(['message' => 'Ошибка при удалении группы и обновлении пользователей'], 500);
+        }
     }
+
 }
