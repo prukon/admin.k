38a95be5574fdf1df4716a855e904f05d4a95fe2
prukon <prukon@gmail.com>
2024-11-02 03:38:52 +0300

    disabled team in [200~AppServiceProvider~

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -1,31 +1,31 @@
 <?php
-
-namespace App\Models;
-
-use App\Models\Traits\Filterable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-
-class Team extends Model
-{
-    use HasFactory;
-    use Filterable;
-    use SoftDeletes;
-
-    protected $table = 'teams'; //явное указание к какой таблице в БД привязана модель
-    protected $guarded = []; //разрешение на изменение данных в таблице
-
-    public function weekdays()
-    {
-        return $this->belongsToMany(Weekday::class, 'team_weekdays','team_id', 'weekday_id' );
-    }
-    public function test() {
-        return "test";
-    }
-    public function users()
-    {
-        return $this->hasMany(User::class);
-    }
-}
+//
+//namespace App\Models;
+//
+//use App\Models\Traits\Filterable;
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\SoftDeletes;
+//
+//
+//class Team extends Model
+//{
+//    use HasFactory;
+//    use Filterable;
+//    use SoftDeletes;
+//
+//    protected $table = 'teams'; //явное указание к какой таблице в БД привязана модель
+//    protected $guarded = []; //разрешение на изменение данных в таблице
+//
+//    public function weekdays()
+//    {
+//        return $this->belongsToMany(Weekday::class, 'team_weekdays','team_id', 'weekday_id' );
+//    }
+//    public function test() {
+//        return "test";
+//    }
+//    public function users()
+//    {
+//        return $this->hasMany(User::class);
+//    }
+//}
