74e1e2d4046510d65f7d90db93d826507520267b
prukon <prukon@gmail.com>
2024-12-04 06:33:50 +0300

    del clients and clients payment

--- database/migrations/2024_12_04_062846_drop_client_tables.php
+++ database/migrations/2024_12_04_062846_drop_client_tables.php
@@ -4,21 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class DropClientTables extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::dropIfExists('client_payments');
+        Schema::dropIfExists('clients');
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::create('client_payments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
-};
+}
