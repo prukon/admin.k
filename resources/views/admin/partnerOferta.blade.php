199abc9ce5b8e48233a86ebed2a4ae5149f7fa63
prukon <prukon@gmail.com>
2025-08-04 17:37:43 +0300

    fix link partner/oferta

--- resources/views/admin/partnerOferta.blade.php
+++ resources/views/admin/partnerOferta.blade.php
@@ -5,4 +5,15 @@
 
     @include('partner-offer-multirasschety')
 
+    <!-- Footer -->
+    <footer class="bg-dark text-white text-center py-4">
+        <div class="container">
+            <p class="mb-1">Все права защищены. 2024 - 2025 кружок.online &copy;</p>
+            <div>
+                <a href="oferta" class="text-white text-decoration-none mx-2">Оферта</a>
+                <a href="{{ route('privacy.policy') }}"  class="text-white text-decoration-none mx-2">Политика конфиденциальности</a>
+            </div>
+        </div>
+    </footer>
+
 @endsection
