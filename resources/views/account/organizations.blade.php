18208745859fe21ad19d3990b2644730ad2c0587
prukon <prukon@gmail.com>
2025-12-12 12:35:18 +0300

    fix oferta

--- resources/views/account/organizations.blade.php
+++ resources/views/account/organizations.blade.php
@@ -175,7 +175,6 @@
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
-    
     <script>
         document.addEventListener('DOMContentLoaded', function () {
 //Обновление партнера
